ff311630f2497de512e1c96eb39a4dbdf4edb121
LawanSouza <146399438+LawanSouza@users.noreply.github.com>
2023-11-21 21:34:34 -0300

    CaixaEletronico.java

--- CaixaEletronico.java
+++ CaixaEletronico.java
@@ -1,21 +1,19 @@
 package caixaEletronico;
-
 import java.util.Scanner;
 
 public class CaixaEletronico {
-
 	public static void main(String[] args) {
 		
 		Scanner sc= new Scanner(System.in);
 		System.out.println("Caixa Eletrônico Braços Curtos da UniPaulistana");
 		System.out.println("");
 		System.out.println("");
-		System.out.println("1 - Abastecimento");
+		System.out.println("1: Abastecimento");
 		System.out.println("");
-		System.out.println("2 - Operações Financeiras");
+		System.out.println("2: Operações Financeiras");
 		System.out.println("");
 		System.out.println("");
-		System.out.println("Digite a opção desejada:");
+		System.out.println("Qual a opção desejada?");
 		
 		int opcao=sc.nextInt();
 		
@@ -23,14 +21,15 @@ public class CaixaEletronico {
 		case 1:
 			System.out.println("Você escolheu a opção Abstecimento");
 			break;
+				
 		case 2:
 			System.out.println("Você escolheu a opção Operações Financeiras");
 			break;
+				
 		default:
-			System.out.println("A opção escolhida é inexistente!\nSelecione apenas 1 ou 2");
+			System.out.println("A opção escolhida é inexistente!" + \n + "Selecione apenas 1 ou 2");
 			break;
-		}
-		
+		}		
 		sc.close();
 	}
 
